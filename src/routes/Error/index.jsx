dff8509e9e0f24ad84408ad307e3462c05fbfc9f
GEdO23 <rm99632@fiap.com.br>
2023-10-25 10:43:29 -0300

    Adcionando conteudo a página error.

--- src/routes/Error/index.jsx
+++ src/routes/Error/index.jsx
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function Error() {
   return (
-    <div>Error-404</div>
+    <div className="error-container">
+      <h1>ERROR - 404</h1>
+      <h2>We couldn't find the page you're looking for...</h2>
+      <Link to='/' className="turnback-link">Go back to Home</Link>
+    </div>
   )
 }
