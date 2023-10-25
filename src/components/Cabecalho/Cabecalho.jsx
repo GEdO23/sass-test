86469be5abe7fb1b24d875a7dd41682ad1fe60a9
GEdO23 <rm99632@fiap.com.br>
2023-10-25 12:03:24 -0300

    Estilizando cabecalho com module.scss

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -1,18 +1,19 @@
 import { Link } from "react-router-dom";
+import style from './Cabecalho.module.scss';
 
 export default function Cabecalho() {
   return (
-    <header className="cabecalho-container">
-        <h1 className="cabecalho-titulo">Portifolio</h1>
-        <ul className="links-list">
-            <li className="links-item">
-                <Link to='/' className="link">About</Link>
+    <header className={style.cabecalhoContainer}>
+        <h1 className={style.cabecalhoTitulo}>Portifolio</h1>
+        <ul className={style.linksList}>
+            <li className={style.linksItem}>
+                <Link to='/' className={style.link}>About</Link>
             </li>
-            <li className="links-item">
-                <Link to='/portfolio' className="link">Portfolio</Link>
+            <li className={style.linksItem}>
+                <Link to='/portfolio' className={style.link}>Portfolio</Link>
             </li>
-            <li className="links-item">
-                <Link to='/contact' className="link">Contact</Link>
+            <li className={style.linksItem}>
+                <Link to='/contact' className={style.link}>Contact</Link>
             </li>
         </ul>
     </header>
