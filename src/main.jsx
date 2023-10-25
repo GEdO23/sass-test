25fac06dff3d54d9b692946b91d1f21a3a55aa50
GEdO23 <rm99632@fiap.com.br>
2023-10-25 08:56:56 -0300

    Configurando as rotas home e error.

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App/App";
 import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Error from "./routes/Error";
+import Home from "./routes/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/", element: <Home />
+      }
+    ],
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
